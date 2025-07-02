906894cc7849362cdb52543f459772d590ebe373
sagorsohel <sohel19sagor@gmail.com>
2025-07-03 04:46:00 +0600

    Add colors dependency and update server listen log formatting

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const morgan=require('morgan');
 const cors=require('cors');
 const dotenv=require('dotenv');
-
+const colors=require('colors');
 const app=express();
 
 
@@ -16,6 +16,11 @@ app.use(cors());
 // Enable CORS for all routes
 dotenv.config();
 // Load environment variables from .env file
+
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`.bgBrightWhite.cyan.bold);
+});
 
 
 
