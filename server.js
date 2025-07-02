4aa2b03bab58ef4479498da448c11d759870de9c
sagorsohel <sohel19sagor@gmail.com>
2025-07-02 23:16:10 +0600

    Add dotenv configuration to load environment variables

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const morgan=require('morgan');
 const cors=require('cors');
+const dotenv=require('dotenv');
 
 const app=express();
+
+
 
 // Middleware
 app.use(express.json());    
@@ -10,5 +13,10 @@ app.use(express.json());
 app.use(morgan('dev'));
 // Log requests to the console
 app.use(cors());
+// Enable CORS for all routes
+dotenv.config();
+// Load environment variables from .env file
+
+
 
 
