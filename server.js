b31069aedf7fb087c94a05b34490b39b5d385fad
sagorsohel <sohel19sagor@gmail.com>
2025-07-03 05:46:27 +0600

    Refactor server.js to improve code organization and readability

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const ConnectDb = require("./services/DBConnect");
-const app = express();
 const router = require("./routes/index");
+
+// Create an Express application
+const app = express();
+
+
+
 // Middleware
+
 app.use(express.json());
 // Parse JSON bodies
 app.use(morgan("dev"));
@@ -16,16 +22,27 @@ app.use(cors());
 dotenv.config();
 // Load environment variables from .env file
 
+
+
+
 // Database connection
 ConnectDb();
+
+
 
 // Routes
 app.use(router);
 
+
+// Serve a welcome message at the root URL
+// This is the main entry point of the API
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Restaurant App API</h1>");
 });
 
+
+
+// Start the server
 app.listen(process.env.PORT || 8000, () => {
   console.log(
     `Server is running on port ${process.env.PORT || 3000}`.bgBrightWhite.cyan
