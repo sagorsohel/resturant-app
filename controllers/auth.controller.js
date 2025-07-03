91b629d0c8446c05db6406f3258422b8c0a76c75
sagorsohel <sohel19sagor@gmail.com>
2025-07-03 06:26:32 +0600

    Add user existence check in registration and update bcrypt dependency

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,6 +21,17 @@ const registerController = async (req, res) => {
       return sendErrorResponse(res,400,  "The following fields are required: " + missingFields.join(", "),
     { missingFields });
     }
+
+    // Check if the user already exists
+    const existingUser = await User.findOne(email);
+    if (existingUser) {
+      return sendErrorResponse(res, 400, "User already exists with this email");
+    }
+
+
+    
+
+
   } catch (error) {
     console.error("Error during registration:", error);
     return sendErrorResponse(res,500, "Internal server error", )
