6309e2d30796cd3af86f8cfb67e69b32be6d1e01
sagorsohel <sohel19sagor@gmail.com>
2025-07-03 08:19:42 +0600

    Add token verification middleware to authenticate users

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const jwt =require('jsonwebtoken');
+const User = require('../models/user.model');
+
+
+const verifyToken = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id);
+
+        if (!user) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+module.exports = verifyToken;
